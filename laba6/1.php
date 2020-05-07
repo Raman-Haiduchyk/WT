4f581e5ad455500d0c683ff169c0970aad6ccbe4
haid_roman <haidroman@gmail.com>
2020-05-07 13:48:37 +0300

    laba6 created

---
+++ laba6/1.php
@@ -0,0 +1,16 @@
+<?php include 'script.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href="index.php">Главная</a></li>
+        <li><a href="2.php">Страница 2</a></li>
+    </ul>
+    <?php getHistory()?>
+</body>
+</html>
